2c6c24d472716782886bc94a2d94882ef6c491f9
bibitto <bibitto.crypto@gmail.com>
2022-09-14 10:25:08 +0900

    create contract

--- contracts/etherWallet.sol
+++ contracts/etherWallet.sol
@@ -1,6 +1,26 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// Import this file to use console.log
-import "../node_modules/hardhat/console.sol";
+import 'hardhat/console.sol';
 
+
+contract EtherWallet {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+        console.log(owner);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint _amount) external {
+        require(msg.sender == owner, "caller is not owner");
+        payable(msg.sender).transfer(_amount);
+        console.log('You have done withdraw');
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+}
